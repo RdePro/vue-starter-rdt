5ca11542be5b1fb2468b32350b53210c923cee1c
hale <hzhayue@corp.netease.com>
2019-05-15 12:53:29 +0800

    feat: save

--- rdc.config.js
+++ rdc.config.js
@@ -1,20 +1,32 @@
 module.exports = {
-    framework: 'vue',
-    docs: {
-        url: 'https://rdepro.github.io/rdc-vue-starter/'
+    "framework": "vue",
+    "docs": {
+        "url": "https://rdepro.github.io/rdc-vue-starter/",
+        "userStyles": [
+            'https://unpkg.com/element-ui/lib/theme-chalk/index.css'
+        ],
+        "userScripts": [
+            'https://unpkg.com/element-ui/lib/index.js'
+        ]
     },
-    render: {
-        includes: ['.html'],
-        mock: {
-            title: 'Hello World'
+    "render": {
+        "includes": [".html"],
+        "mock": {
+            "title": "Hello World"
         }
     },
-    mappings: [
-        { from: 'app/App.vue', to: 'src/App.vue' },
-        { from: 'app/components', to: 'src/components' },
-    ],
-    docker: {
-        tag: 'rdebase/rdc-vue-starter:0.0.1-alpha.1',
-        ports: ['8080:8080']
+    "mappings": [{
+        "from": "app/App.vue",
+        "to": "src/App.vue"
+    }, {
+        "from": "app/components",
+        "to": "src/components"
+    }],
+    "docker": {
+        "tag": "rdebase/rdc-vue-starter:0.0.1-alpha.1",
+        "ports": ["8080:8080"]
     },
-};
+    "dev": {
+        suites: []
+    },
+}
--- template/src/main.js
+++ template/src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
-
-abc(123)
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
