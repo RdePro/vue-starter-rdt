f85de16059a0d6200c07f36da97ae49a95f49139
hale <hzhayue@corp.netease.com>
2019-05-16 20:59:03 +0800

    misc: save

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node:latest
-
-WORKDIR /usr/rde/rdebase/rdc-vue-starter
-
-COPY . .
--- rdc.config.js
+++ rdc.config.js
@@ -1,37 +1,27 @@
 module.exports = {
-    framework: 'vue',
-    docs: {
-        url: 'https://rdepro.github.io/rdc-vue-starter/',
-        userStyles: [
-            'https://unpkg.com/element-ui/lib/theme-chalk/index.css'
-        ],
-        userScripts: [
-            'https://unpkg.com/element-ui/lib/index.js'
-        ]
+    "framework": "vue",
+    "docs": {
+        "url": "https://rdepro.github.io/rdc-vue-starter/",
+        "userStyles": ["https://unpkg.com/element-ui/lib/theme-chalk/index.css"],
+        "userScripts": ["https://unpkg.com/element-ui/lib/index.js"]
     },
-    render: {
-        includes: ['.html', '.js'],
-        mock: {
-            title: 'Hello World'
-        },
-        dev: {
-            render: {
-                suites: [
-                    {
-                        name: 'sc-common-vue',
-                        version: '^0.1.30',
-                        alias: 'scCommonVue'
-                    }
-                ],
+    "render": {
+        "includes": [".html", ".js"],
+        "dev": {
+            "render": {
+                "title": "title"
             }
         }
     },
-    mappings: [
-        { from: 'app/App.vue', to: 'src/App.vue' },
-        { from: 'app/components', to: 'src/components' },
-    ],
-    docker: {
-        tag: 'rdebase/rdc-vue-starter:0.0.1-alpha.5',
-        ports: ['8080:8080']
+    "mappings": [{
+        "from": "app/App.vue",
+        "to": "src/App.vue"
+    }, {
+        "from": "app/components",
+        "to": "src/components"
+    }],
+    "docker": {
+        "tag": "rdebase/rdc-vue-starter:0.0.1-alpha.7",
+        "ports": ["8080:8080"]
     }
-};
+}
