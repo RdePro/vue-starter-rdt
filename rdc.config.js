82a1a2f99d22c3b591bf9826bcbb28c413d5e8c9
hale <hzhayue@corp.netease.com>
2019-05-22 15:03:48 +0800

    feat: init

--- rdc.config.js
+++ rdc.config.js
@@ -1,27 +1,39 @@
 module.exports = {
-    "framework": "vue",
-    "docs": {
-        "url": "https://rdepro.github.io/rdc-vue-starter/",
-        "userStyles": ["https://unpkg.com/element-ui/lib/theme-chalk/index.css"],
-        "userScripts": ["https://unpkg.com/element-ui/lib/index.js"]
-    },
-    "render": {
-        "includes": [".html", ".js"],
-        "dev": {
-            "render": {
-                "title": "title"
-            }
-        }
-    },
-    "mappings": [{
-        "from": "app/App.vue",
-        "to": "src/App.vue"
-    }, {
-        "from": "app/components",
-        "to": "src/components"
-    }],
-    "docker": {
-        "tag": "rdebase/rdc-vue-starter:0.0.1-alpha.7",
-        "ports": ["8080:8080"]
+  framework: 'vue',
+  docs: {
+    url: 'https://rdepro.github.io/rdc-vue-starter/',
+    userStyles: [
+      'https://unpkg.com/element-ui/lib/theme-chalk/index.css'
+    ],
+    userScripts: [
+      'https://unpkg.com/element-ui/lib/index.js'
+    ]
+  },
+  render: {
+    includes: [
+      '.html',
+      '.js'
+    ],
+    dev: {
+      render: {
+        title: 'title'
+      }
     }
+  },
+  mappings: [
+    {
+      from: 'app/App.vue',
+      to: 'src/App.vue'
+    },
+    {
+      from: 'app/components',
+      to: 'src/components'
+    }
+  ],
+  docker: {
+    tag: 'rdebase/rdc-vue-starter:0.0.1-beta.0',
+    ports: [
+      '8080:8080'
+    ]
+  }
 }
